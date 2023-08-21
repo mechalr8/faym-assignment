d526082e6ddbbd38ffa23a88c6e0f1333a3f75c0
mechalr8 <harsitagrawal59@gmail.com>
2023-08-21 23:59:56 +0530

    fixed url

--- src/components/VideoContainer.js
+++ src/components/VideoContainer.js
@@ -2,12 +2,10 @@ import React from "react";
 
 const VideoContainer = ({ text }) => {
     function getId(url) {
-        const id = url.split("&")[0].split("=")[1];
-        // const regExp =
-        //     /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|&v=)([^#&?]*).*/;
-        // const match = url.match(regExp);
-        // return match && match[2].length === 11 ? match[2] : null;
-        return id;
+        const regExp =
+            /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|&v=)([^#&?]*).*/;
+        const match = url.match(regExp);
+        return match && match[2].length === 11 ? match[2] : null;
     }
 
     const videoId = getId(text);
